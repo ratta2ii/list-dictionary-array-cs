02112710d551eb734f0ca76b618074377f2d475c
Trevor Mackin <example@example.com>
2019-12-12 16:06:51 -0800

    Add examples of both a string array and an integar array.

---
+++ Array.cs
@@ -0,0 +1,17 @@
+
+---------------------   STRING ARRAY   -----------------------------
+string[] fruits = { "apples", "bananas", "oranges", "grapes" };
+
+fruits;
+// {"apples", "bananas", "oranges", "grapes"}
+
+
+---------------------   INT ARRAY   -----------------------------
+int[] primeNumbers = { 2, 3, 5, 7, 11 };
+
+primeNumbers;
+// {2, 3, 5, 7, 11}
+
+
+---------------------
+
